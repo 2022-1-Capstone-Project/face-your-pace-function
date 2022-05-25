11fa55138d2f7dc04b09c53e39fb60605cff7d86
Dain Yoon <76734572+yoondain@users.noreply.github.com>
2022-05-25 12:11:42 +0900

    test.py 의 형태로 download_mp3.py 의 함수 불러오면 됨

---
+++ download/test.py
@@ -0,0 +1,6 @@
+from download_mp3 import *
+
+if __name__ == '__main__':
+    download_music('https://soundcloud.com/ferret-lie/only-your-stars-trickstar-ver',r'C:\Users\yoondain\Desktop\00학교\4-1')
+    # soundclound 링크, 저장 위치
+ 
