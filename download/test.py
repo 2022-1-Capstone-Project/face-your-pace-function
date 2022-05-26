3f34b53b06093ad6a7098b46d6861a76d790daf3
Dain Yoon <76734572+yoondain@users.noreply.github.com>
2022-05-26 19:44:23 +0900

    test.py 함수 변경

--- download/test.py
+++ download/test.py
@@ -1,8 +1,7 @@
-from download_mp3 import download_music_mp3
-from download_wav import download_music_wav
+from download_all import download_music_mp3, download_music_wav
 
 if __name__ == '__main__':
     # soundclound 링크, 저장 위치
-    download_music_mp3('https://soundcloud.com/ferret-lie/only-your-stars-trickstar-ver',r'C:\Users\yoondain\Desktop\00학교\4-1')
-    download_music_wav('https://soundcloud.com/ferret-lie/only-your-stars-trickstar-ver',r'C:\Users\yoondain\Desktop\00학교\4-1')
+    # download_music_mp3('https://soundcloud.com/ferret-lie/only-your-stars-trickstar-ver',r'C:\Users\yoondain\Desktop\capstone')
+    download_music_wav('https://soundcloud.com/ferret-lie/only-your-stars-trickstar-ver',r'C:\Users\yoondain\Desktop\capstone')
  
